02b0b8318b282c78558a851b9169578dc03a506b
Chukwuebuka127 <Orjideemma@gmail.com>
2026-07-03 06:55:37 +0100

    fix: update terraform version and backened config for CI

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,9 @@
- terraform {
+terraform {
   backend "s3" {
-    bucket       = "shortify-tfstate-emmanuel127"
-    key          = "shortify/terraform.tfstate"
-    region       = "eu-central-1"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "shortify-tfstate-emmanuel127"
+    key            = "shortify/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "shortify-tf-locks"
+    encrypt        = true
   }
 }
-
-
